6d90e2d002a1a3aec277f6fa3104b98c7f606511
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-08-14 23:49:51 +0100

    Function for finding the next empty cell (0 value). Validation fn checks if legal move can be made.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -25,3 +25,32 @@ def render_grid():
                 print(grid[i][j])
             else:
                 print(f"{grid[i][j]} ",end="")
+
+def next_empty_cell():
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 0:
+                return (i, j)
+    return None
+
+
+def is_valid(digit, coords):
+    # Check row (x,i) 
+    for i in range(len(grid)):
+        if grid[coords[0]][i] == digit and coords[1] != i:
+            return False
+
+    # Check column (i,y) 
+    for i in range(len(grid)):
+        if grid[i][coords[1]] == digit and coords[0] != i:
+            return False
+
+    # Check subgrid 
+    subgrid_x = coords[1] // 3
+    subgrid_y = coords[0] // 3
+
+    for i in range(subgrid_y*3, subgrid_y*3 + 3):
+        for j in range(subgrid_x * 3, subgrid_x*3 + 3):
+            if grid[i][j] == digit and (i,j) != coords:
+                return False
+    return True
